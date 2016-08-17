648864171e20a4b5cf423d93fbc02db7e73448d2
roofner <steelerbaby210@hotmail.com>
2016-08-17 15:23:28 -0400

    more test

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -6,5 +6,10 @@ class TestTicket <Minitest::Test
 			assert_equal({:quarter=> 0, :dime => 0, :nickel => 1, :penny=> 0}, money(5))
 	end
 
-	
+	def test_for_2_quarter_and_4_penny_for_54
+			assert_equal({:quarter=> 2, :dime => 0, :nickel => 0, :penny=> 4}, money(54))
+	end
+	def test_for_1_quarter_1_dime_and_3_penny_for_38
+	assert_equal({:quarter=> 1, :dime => 1, :nickel => 0, :penny=> 3}, money(38))
+	end
 end
