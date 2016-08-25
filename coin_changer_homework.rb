5d891252aa436c1d01817b99219e7846edc5b88d
roofner <steelerbaby210@hotmail.com>
2016-08-25 11:16:11 -0400

    updated coin changer

--- coin_changer_homework.rb
+++ coin_changer_homework.rb
@@ -1,19 +1,23 @@
 def generate_change(cents)
 	change ={}
+	quarter_value = 25
+	dime = 10
+	nickel = 5
+
 		if cents >= 25
-			coins = cents /25
+			coins = cents /quarter_value
 	  	   change[:quarter] = coins 
-	  	   cents = cents - (25 * coins)
+	  	   cents = cents - (quarter_value * coins)
 		end
 	
 		if cents >= 10
-			coins = cents / 10
+			coins = cents / dime
 			change[:dime] = coins
-			cents = cents - (10 * coins)
+			cents = cents - (dime * coins)
 		end
 		if cents >= 5
 			change[:nickel] = 1
-			cents = cents - 5
+			cents = cents - nickel
 		end
 		if cents > 0
 			change[:penny] = cents
